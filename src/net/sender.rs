a91c38a74196b6d6b2d1ad62de477111ce91f9ea
secondary-smiles <shavkinder@gmail.com>
2022-10-17 10:12:29 -0400

    Add threads

--- src/net/sender.rs
+++ src/net/sender.rs
@@ -1,9 +1,32 @@
 use std::net::TcpStream;
 use std::io::prelude::*;
+use std::thread;
+use info_utils::error;
+use info_utils::prelude::*;
 
 use crate::util::{
     comms::Net,
     connection::Connection,
 };
 
-pub fn create_sender_connection(connection: Connection, comms: Net) {}
+pub fn create_sender_connection(connection: Connection, comms: Net) {
+    let mut stream = TcpStream::connect(format!("{}:{}", connection.domain.should("Should be validated"), connection.port.should("Should be validated"))).eval_or_else(|e| {
+        error!("{}", e);
+    });
+
+    let listen_thread = thread::Builder::new()
+        .name("Listener Thread".to_string())
+        .spawn(|| {
+            terror!("test error");
+        }).eval();
+
+
+    let send_thread = thread::Builder::new()
+        .name("Sender Thread".to_string())
+        .spawn(|| {
+            terror!("test error");
+        }).eval();
+
+    listen_thread.join().should("Thread should panic");
+    send_thread.join().should("Thread should panic");
+}

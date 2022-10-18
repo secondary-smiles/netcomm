1c3e2f92765a4ae118181395a42d6dc8c8c058b3
secondary-smiles <shavkinder@gmail.com>
2022-10-18 08:48:20 -0400

    Add broken UI

--- src/repl/print.rs
+++ src/repl/print.rs
@@ -7,16 +7,29 @@ use crate::util::{
 };
 
 pub fn create_repl(comms: Repl) {
-    loop {
-        let incoming_messages: Vec<Message> = comms.recvr.try_iter().collect();
+    let listen_thread = thread::Builder::new()
+        .name("UI Listener".to_string())
+        .spawn(move || {
+            loop {
+                let incoming_messages: Vec<Message> = comms.recvr.try_iter().collect();
 
-        for message in incoming_messages {
-            log!("{}", message)
-        }
+                for message in incoming_messages {
+                    println!("{}", message)
+                }
+            }
+        }).eval();
 
-        let msg = Message {
-            sender: "EXT".to_string(),
-            content: "test message".to_string()
-        };
-    }
+    let send_thread = thread::Builder::new()
+        .name("UI Sender".to_string())
+        .spawn(move || {
+            let msg = Message {
+                sender: "EXT".to_string(),
+                content: "test message".to_string(),
+            };
+
+            comms.sender.send(msg).eval();
+            // thread::sleep(Duration::from_millis(500));
+        }).eval();
+    listen_thread.join().eval();
+    send_thread.join().eval();
 }

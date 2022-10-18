4a8ba60de665afd8717f107a9a75ef2e10fda40d
secondary-smiles <shavkinder@gmail.com>
2022-10-18 09:57:48 -0400

    Add termion & better thread handling

--- src/repl/print.rs
+++ src/repl/print.rs
@@ -1,15 +1,26 @@
 use std::thread;
-use std::time::Duration;
 use info_utils::prelude::*;
+use termion::{
+    screen,
+    raw::{IntoRawMode},
+    input::TermRead,
+    event::Key,
+};
+use std::io::{Write, stdout, stdin};
+
+
 use crate::util::{
     comms::Repl,
     message::Message,
 };
 
 pub fn create_repl(comms: Repl) {
-    thread::Builder::new()
+    let handle_thread = thread::Builder::new()
         .name("UI Listener".to_string())
         .spawn(move || {
+            let mut stdin = termion::async_stdin().keys();
+            let mut stdout = stdout().into_raw_mode().eval();
+
             loop {
                 let incoming_messages: Vec<Message> = comms.recvr.try_iter().collect();
 
@@ -22,8 +33,13 @@ pub fn create_repl(comms: Repl) {
                     content: "test message\r\n".to_string(),
                 };
 
-                comms.sender.send(msg).eval();
-                thread::sleep(Duration::from_millis(1000));
+                let mut should_break = false;
+                comms.sender.send(msg).eval_or_else(|e| {
+                    should_break = true;
+                    warn!("{e}");
+                });
+                if should_break { break; };
             }
         }).eval();
+    handle_thread.join().eval();
 }
